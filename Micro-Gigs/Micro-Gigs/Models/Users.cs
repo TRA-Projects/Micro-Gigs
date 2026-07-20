41e16d733cad35ecf7d93d54def31c1a671ba531
shahad Hamed <shahadhamed1820@gmail.com>
2026-07-20 15:26:12 +0400

    Add User model with validation attributes

--- Micro-Gigs/Micro-Gigs/Models/Users.cs
+++ Micro-Gigs/Micro-Gigs/Models/Users.cs
@@ -1,6 +1,34 @@
-﻿namespace Micro_Gigs.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Micro_Gigs.Models
 {
+    [Table("Users")]
     public class Users
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string UserType { get; set; } // Client, Freelancer
+
+        [Required]
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+
+
+        // Navigation Properties
+
+
     }
 }

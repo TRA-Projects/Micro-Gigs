058b7a12719459c088af4d7be6514d72073f7773
“Aaisha <alsaidiaaisha@gmail.com>
2026-07-21 09:47:09 +0400

    Update GIG_ASSIGNMENTS Models

--- Micro-Gigs/Micro-Gigs/Models/GigAssignments.cs
+++ Micro-Gigs/Micro-Gigs/Models/GigAssignments.cs
@@ -3,17 +3,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Micro_Gigs.Models
 {
+    [Table("GIG_ASSIGNMENTS")]
     public class GigAssignments
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int AssignmentId { get; set; }
 
-        [ForeignKey(nameof(Gigs))]
+        //[ForeignKey(nameof(Users))]
+        [Required]
         public int GigId { get; set; }
-       
+        
+        [Required]
         public int freelancerId { get; set; }
         public decimal AgreedPrice { get; set; }
-        public  string Status { get; set; }
+
+        public DateTime AssignedDate { get; set; }
+
+        public DateTime? CompletionDate { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public  string Status { get; set; }//InProgress, Submitted, Approved, Rejected, Completed
     }
 }

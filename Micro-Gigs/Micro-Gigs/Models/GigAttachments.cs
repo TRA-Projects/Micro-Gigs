495841840a174ead7e8df08f8972cad8c39315cf
Ebtesam <ebtesamobaid123@gmail.com>
2026-07-21 10:25:54 +0400

    Add Navigation properties (Relationships) in GigAttachments and GigReviews .

--- Micro-Gigs/Micro-Gigs/Models/GigAttachments.cs
+++ Micro-Gigs/Micro-Gigs/Models/GigAttachments.cs
@@ -9,15 +9,24 @@ namespace Micro_Gigs.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid AttachmentId { get; set; } = Guid.NewGuid(); // system generated — Primary Key
+        public  int AttachmentId { get; set; } // system generated — Primary Key
 
         [Required]
-        public Guid GigId { get; set; }                         // system generated — linked to Gigs (Foreign Key)
+        public int GigId { get; set; }                       // system generated — linked to Gigs (Foreign Key)
 
         [Required]
-        public string FileUrl { get; set; }                     // system generated — path or URL to the file
+        public string FileUrl { get; set; }                 // system generated — path or URL to the file
 
         [Required]
-        public Guid User ID { get; set; }                    // system generated — User ID of the uploader (Foreign Key)
+        public int UserID { get; set; }                    // system generated — User ID of the uploader (Foreign Key)
+
+        //------------------------------------------------------------------------------------------------------------------------------//
+        // Navigation properties (Relationships)
+
+        [ForeignKey("GigId")]
+        public Gigs Gig { get; set; }  // Reference to the specific gig
+
+        [ForeignKey("UserID")]
+        public Users Uploader { get; set; }  // Reference to the user who uploaded the file
     }
 }

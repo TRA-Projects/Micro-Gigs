3d8c9f710bb9a3bfb3b50ad06ffbaf63483b89eb
“Noura” <noura.almaskari11@gmail.com>
2026-07-21 09:18:59 +0400

    adding main properties of gig categories model

--- Micro-Gigs/Micro-Gigs/Models/GigCategories.cs
+++ Micro-Gigs/Micro-Gigs/Models/GigCategories.cs
@@ -1,6 +1,19 @@
-﻿namespace Micro_Gigs.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Micro_Gigs.Models
 {
+    [Table("Gig_Categories")]
     public class GigCategories
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GigCategoryId { get; set; }
+
+        [Required] 
+        public string CategoryName { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
     }
 }

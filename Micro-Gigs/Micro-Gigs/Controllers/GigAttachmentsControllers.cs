2a03825ef426414d534694b814696525ae19aa00
Ebtesam <ebtesamobaid123@gmail.com>
2026-07-21 16:30:35 +0400

    updates

--- Micro-Gigs/Micro-Gigs/Controllers/GigAttachmentsControllers.cs
+++ Micro-Gigs/Micro-Gigs/Controllers/GigAttachmentsControllers.cs
@@ -4,23 +4,6 @@ using Micro_Gigs.DTOs;
 
 namespace Micro_Gigs.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class GigAttachmentsController : ControllerBase
-    {
-        private readonly GigAttachmentsService _service;
-
-        public GigAttachmentsController(GigAttachmentsService service)
-        {
-            _service = service;
-        }
-
-        [HttpPost("upload")]
-        public async Task<IActionResult> Upload(GigAttachmentsInputDTOs input)
-        {
-            var userId = Guid.NewGuid(); //  حتى نربطه بنفاذ
-            await _service.CreateAttachment(input, userId);
-            return Ok("Upload successful.");
-        }
-    }
+    
 }
+    
--- Micro-Gigs/Micro-Gigs/Services/GigAttachmentsServices.cs
+++ Micro-Gigs/Micro-Gigs/Services/GigAttachmentsServices.cs
@@ -4,26 +4,13 @@ using Micro_Gigs.Repositories;
 
 namespace Micro_Gigs.Services
 {
-    public class GigAttachmentsService
+    public class GigAttachmentsServices
     {
-        private readonly GigAttachmentsRepo _repo;
-
-        public GigAttachmentsService(GigAttachmentsRepo repo)
-        {
-            _repo = repo;
-        }
+        
 
         public async Task CreateAttachment(GigAttachmentsInputDTOs input, Guid userId)
         {
-            //var newFile = new GigAttachments
-            //{
-            //    AttachmentId = Guid.NewGuid(),
-            //    GigId = input.GigId,
-            //    FileUrl = input.FileUrl,
-            //    UploadedBy = userId
-            //};
-
-            //await _repo.AddAsync(newFile);
+           
         }
     }
 }

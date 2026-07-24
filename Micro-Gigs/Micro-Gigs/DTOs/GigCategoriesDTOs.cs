da28e13480cf016e0cc8ebabefd44d053c7cc0a6
Noura Almaskari <noura.almaskari11@gmail.com>
2026-07-24 13:13:48 +0400

    adding Gigs and GigsCategiory DTOs

--- Micro-Gigs/Micro-Gigs/DTOs/GigCategoriesDTOs.cs
+++ Micro-Gigs/Micro-Gigs/DTOs/GigCategoriesDTOs.cs
@@ -1,9 +1,21 @@
-﻿namespace Micro_Gigs.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Micro_Gigs.DTOs
 {
-    public class GigCategoriesInputDTOs
+    public class CreateCategoryDto
     {
+        [Required(ErrorMessage = "CategoryName is required")]
+        [MaxLength(100)]
+        public string CategoryName { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+
     }
-    public class GigCategoriesOutputDTOs
+    public class CategoryResponseDto
     {
+        public int GigCategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }

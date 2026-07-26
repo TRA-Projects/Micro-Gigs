ef8b4526a6c7193580a0ce8f73558f8fa34ce3b4
shahad Hamed <shahadhamed1820@gmail.com>
2026-07-26 09:10:08 +0400

    add input and output DTOs

--- Micro-Gigs/Micro-Gigs/DTOs/UsersDTOs.cs
+++ Micro-Gigs/Micro-Gigs/DTOs/UsersDTOs.cs
@@ -1,10 +1,38 @@
-﻿namespace Micro_Gigs.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Micro_Gigs.DTOs
 {
     public class UsersInputDTOs
     {
+        // Register
+        [Required(ErrorMessage = "Username is required")]
+        [MaxLength(100)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "User Type is required")]
+        [MaxLength(20)]
+        public string UserType { get; set; } = string.Empty; // Client or Freelancer
     }
 
     public class UsersOutputDTOs
     {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string UserType { get; set; } = string.Empty;
+
+        public DateTime RegistrationDate { get; set; }
     }
 }

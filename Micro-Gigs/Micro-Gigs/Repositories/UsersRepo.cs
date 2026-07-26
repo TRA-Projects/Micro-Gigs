1decb10fe0846b9c71d347172e60a505604b9f47
shahad Hamed <shahadhamed1820@gmail.com>
2026-07-26 09:27:15 +0400

    Add user repo

--- Micro-Gigs/Micro-Gigs/Repositories/UsersRepo.cs
+++ Micro-Gigs/Micro-Gigs/Repositories/UsersRepo.cs
@@ -1,6 +1,75 @@
-﻿namespace Micro_Gigs.Repositories
+﻿using Micro_Gigs.Models;
+
+namespace Micro_Gigs.Repositories
 {
     public class UsersRepo
     {
+
+        private MicroGigsContext _context;
+
+
+        // Constructor Injection: DbContext from DI Container
+        public UsersRepo(MicroGigsContext context)
+        {
+            _context = context;
+        }
+
+        // ============================
+        // READ: Get All Users
+        // ============================
+        public List<Users> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+
+        // ============================
+        // READ: Get User By ID
+        // ============================
+        public Users? GetById(Guid id)
+        {
+            return _context.Users
+                .FirstOrDefault(u => u.UserId == id);
+        }
+
+
+        // ============================
+        // READ: Get User By Email
+        // ============================
+        public Users? GetByEmail(string email)
+        {
+            return _context.Users
+                .FirstOrDefault(u => u.Email == email);
+        }
+
+
+        // ============================
+        // CREATE: Add New User
+        // ============================
+        public void Add(Users user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+
+        // ============================
+        // UPDATE: Update Existing User
+        // ============================
+        public void Update(Users user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+
+        // ============================
+        // DELETE: Delete User
+        // ============================
+        public void Delete(Users user)
+        {
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
     }
 }
